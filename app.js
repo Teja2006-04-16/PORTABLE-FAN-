1b593c7b2f9cb180a70f1297e0a065b4c8a0ce05
PALAKEERTHI TEJA <palakeerthiteja16@gmail.com>
2025-03-19 22:47:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var on_fan = document.getElementById("fanon");
+var off_fan = document.getElementById("fanoff");
+var fan = document.getElementById("fan");
+function fon() {
+    fan.classList.add("rotate");
+}
+function foff() {
+    fan.classList.remove("rotate");
+}
